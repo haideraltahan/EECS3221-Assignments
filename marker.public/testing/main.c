482b11af2f21af9a492eeffbf3b07927d26f98f1
BryanEmbree <43036071+BryanEmbree@users.noreply.github.com>
2019-05-20 20:12:32 -0400

    Add files via upload

---
+++ marker.public/testing/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include "fork.h"
+
+int was_alarm=0;
+
+/* The main program does much of the work. parses the command line arguments */
+/* sets up the alarm and the alarm signal handler opens the files and pipes */
+/* for redirection etc., invoke start_child, closes files that should be  */
+/* closed, waits for the children to finish and reports their status */
+/* or exits printing a message and kill its children if they do not die */
+/* in time (just the parent exiting may not be enough to kill the children) */
+int main(int argc, char *argv[]) {
+
+	if (argc < 4) {
+		printf("Too few arguments given. There must be at least 4.\n");
+		exit(-1);
+	}
+	char *p_position = NULL; //start looking at the third argument (after ./marker)
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "-p-") == 0) {
+			p_position = argv[i];
+			argv[i] = (char *) NULL;
+			break;
+		}
+
+	}
+	if (!p_position) {
+		printf("-p- was not found as an argument.\n");
+		exit(-1);
+	}
+	if (p_position == argv[1] || p_position == argv[argc - 1]) {
+		printf("-p- cannot be the first or last argument. It must connect two other commands.\n");
+		exit(-1);
+	}
+
+	printf("%d\n", p_position);
+	int fd[2];
+	pid_t pid;
+	remove("test.out"); //remove previous files
+	int in_file = open("test.in", O_RDWR | O_CREAT, 0666); //create the ones we need to read/write
+	int out_file = open("test.out", O_RDWR | O_CREAT, 0666);
+	printf("%d %d\n", in_file, out_file);
+
+	if (pipe(fd) < 0) {
+		printf("Pipe creation error\n");
+		exit(-1);
+	}
+
+	if ((pid = fork()) < 0) {
+		printf("Fork error.\n");
+		exit(-1);
+	}
+	if (pid == 0) { //Child
+		printf("Child\n");
+		close(fd[0]);
+		close(0);
+		dup(3);
+		close(1);
+		dup(4);
+		close(2);
+		close(3); 
+		close(4);
+		char *cmd[] = {"wc", NULL};
+		execvp(cmd[0], cmd);
+
+	}
+	if (pid > 0) {//Parent
+		printf("Parent\n");
+		close(fd[1]);
+		close(3); close(4);
+		wait();
+	}
+	exit(0);
+	
+	
+}
