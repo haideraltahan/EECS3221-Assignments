52f270745ad7fecb6143449acfa23813fb3454bd
BryanEmbree <43036071+BryanEmbree@users.noreply.github.com>
2019-05-21 13:25:10 -0400

    Add files via upload

--- marker.public/testing/main.c
+++ marker.public/testing/main.c
@@ -20,10 +20,10 @@ int main(int argc, char *argv[]) {
 		printf("Too few arguments given. There must be at least 4.\n");
 		exit(-1);
 	}
-	char *p_position = NULL; //start looking at the third argument (after ./marker)
+	char p_position = 0; //start looking at the third argument (after ./marker)
 	for (int i = 0; i < argc; i++) {
 		if (strcmp(argv[i], "-p-") == 0) {
-			p_position = argv[i];
+			p_position = i;
 			argv[i] = (char *) NULL;
 			break;
 		}
@@ -38,44 +38,92 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
-	printf("%d\n", p_position);
+	printf("p position: %d\n", p_position);
+	char *command1[p_position];
+	char *command2[argc - p_position];
+	int current = 1;
+	while (current < p_position) {
+		command1[current - 1] = argv[current];
+		printf("%s\n", command1[current - 1]);
+		printf("%d\n", current);
+		current++;
+	}
+	command1[current - 1] = (char *) NULL;
+
+	current = 0;
+	while (p_position + 1 + current < argc) {
+		command2[current] = argv[p_position + 1 + current];
+		printf("%s\n", command2[current]);
+		printf("%d\n", current);
+		current++;
+	}
+	command2[current] = (char *) NULL;
+	
+
 	int fd[2];
-	pid_t pid;
-	remove("test.out"); //remove previous files
+	pid_t pid1, pid2, wid;
+	remove("test.out"); remove("error.in"); remove("error.out");//remove previous files
 	int in_file = open("test.in", O_RDWR | O_CREAT, 0666); //create the ones we need to read/write
 	int out_file = open("test.out", O_RDWR | O_CREAT, 0666);
-	printf("%d %d\n", in_file, out_file);
+	int in_error = open("error.in", O_RDWR | O_CREAT, 0666);
+	int out_error = open("error.out", O_RDWR | O_CREAT, 0666);
+	char buffer[1000] = {};
+	printf("%d %d %d %d\n", in_file, out_file, in_error, out_error);
 
 	if (pipe(fd) < 0) {
 		printf("Pipe creation error\n");
 		exit(-1);
 	}
 
-	if ((pid = fork()) < 0) {
-		printf("Fork error.\n");
+	pid1 = fork();
+	if (pid1 < 0) {
+		printf("Fork pid1 failed.\n");
 		exit(-1);
 	}
-	if (pid == 0) { //Child
-		printf("Child\n");
+	if (pid1 == 0) {
+		printf("pid1 forked.\n");
 		close(fd[0]);
-		close(0); //close stdin
-		dup(in_file); //replace it with in_file
-		close(1); //close stdout
-		dup(out_file); //replace with out_file
-		close(2); //close stderror for now
-		close(in_file);
-		close(out_file);
+		if (dup2(in_file, 0) < 0 || dup2(fd[1], 1) < 0) {
+			printf("pid1 failed dup2.\n");
+			exit(-1);
+		}
+		close(fd[1]); close(in_file);
 		char *cmd[] = {"wc", NULL};
-		execvp(cmd[0], cmd);
+		execvp(command1[0], command1);
+	}
+	
+	pid2 = fork();
+	if (pid2 < 0) {
+		printf("Fork pid2 failed.\n");
+		exit(-1);
+	}
+	if (pid2 == 0) {
+		printf("pid2 forked.\n");
+		close(fd[1]);
+		//read(fd[0], buffer, sizeof(buffer));
+		//printf("Here is the buffer's contents: %s", buffer);
+		if (dup2(fd[0], 0) < 0 || dup2(out_file, 1) < 0) {
+			printf("pid2 failed dup2\n");
+		}
+		close(fd[0]); close(out_file);
+		char *cmd[] = {"cat", NULL};
+		execvp(command2[0], command2);
 
 	}
-	if (pid > 0) {//Parent
-		printf("Parent\n");
-		wait();
-		close(fd[1]);
-		close(3); close(4);
-	}
-	exit(0);
+	close(fd[0]); close(fd[1]);
+	/*
+	wid = wait(NULL);
+  	if (wid == pid1)
+    	printf("Process %d (%s) finished\n",wid, argv[1]);
+  	if (wid == pid2)
+    	printf("Process %d (%s) finished\n",wid, argv[p_position]);
+  	wid = wait(NULL);
+  	if (wid == pid1)
+    	printf("Process %d (%s) finished\n",wid, argv[1]);
+  	if (wid == pid2)
+    	printf("Process %d (%s) finished\n",wid, argv[p_position]);
+    */
+
 	
 	
 }
