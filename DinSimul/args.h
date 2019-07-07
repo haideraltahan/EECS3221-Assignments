72a2a4ee91c52445ab2c6ae07838351dd7978520
Haider Al-Tahan <haideraltahan@gmail.com>
2019-07-07 13:55:43 -0400

    Now program accepts arguements

--- DinSimul/args.h
+++ DinSimul/args.h
@@ -1,7 +1,6 @@
 struct thread_arg
 {
-  int N, T;
-  float lam, mu;
+  int chops[2];
   pthread_cond_t *start_line, *chair;	/* find a better name */
   pthread_mutex_t *mutex;
   unsigned int seed;		/* Needed for the random number genarator */
--- DinSimul/simul.c
+++ DinSimul/simul.c
@@ -19,6 +19,8 @@ int nblocked;            /* The number of threads blocked */
 int nThink, nHungry, nEating = 0;
 enum STATES{THINKING, HUNGRY, EATING};
 int chopsticks[] = {1,1,1,1,1};
+pthread_cond_t chopsticks_cond[5];
+pthread_cond_t chairs_cond[5];
 
 /***********************************************************************
                          P H I L O S O P H E R
@@ -36,11 +38,12 @@ void *philosopher(void *vptr) {
         if (pthrerr != 0)
             fatalerr("Client", pthrerr, "Mutex lock failed\n");
         nblocked++;
-        if (nblocked == ptr->N) {            /* cond_signal never returns an error code */
+        if (nblocked == N - 1) {            /* cond_signal never returns an error code */
             pthrerr = pthread_cond_signal(ptr->chair);
             if (pthrerr != 0)
                 fatalerr("Client", 0, "Condition signal failed\n");
         }
+        printf("Thread id: %d, start clock, STATE: %d\n", pthread_self(),state);
         pthrerr = pthread_cond_wait(ptr->start_line, ptr->mutex);
         if (pthrerr != 0)
             fatalerr("Client", 0, "Condition wait failed\n");
@@ -49,22 +52,41 @@ void *philosopher(void *vptr) {
             nThink += 1;
             // if less than lamda then he switch to hungry state
             // need to go EATING when the chopsticks become available.
-            if ( rand0_1(&(ptr->seed)) < ptr->lam) {
+            if ( rand0_1(&(ptr->seed)) < lam) {
                 state = HUNGRY;
             }
         }else if(state == HUNGRY){
             nHungry += 1;
             // wait for chopstick
-            if (chopsticks[ptr->seed % N] && chopsticks[(ptr->seed + 1) % N]){
-                chopsticks[ptr->seed % N] = chopsticks[(ptr->seed + 1) % N] = 0;
+            if (chopsticks[ptr->chops[0]] && chopsticks[ptr->chops[1]]){
+                chopsticks[ptr->chops[0]] = chopsticks[ptr->chops[1]] = 0;
                 state = EATING;
+            }else{
+                nblocked++;
+                if (!chopsticks[ptr->chops[0]]) {
+                    pthrerr = pthread_cond_wait(&chopsticks_cond[ptr->chops[0]], ptr->mutex);
+                    if (pthrerr != 0)
+                        fatalerr("Client", 0, "Condition wait failed\n");
+                }
+                if (!chopsticks[ptr->chops[1]]) {
+                    pthrerr = pthread_cond_wait(&chopsticks_cond[ptr->chops[1]], ptr->mutex);
+                    if (pthrerr != 0)
+                        fatalerr("Client", 0, "Condition wait failed\n");
+                }
             }
-        }else if(state == EATING && rand0_1(&(ptr->seed)) < ptr->mu){
+        }else if(state == EATING && rand0_1(&(ptr->seed)) < mu){
             nEating += 1;
             // If less than mu, then we switch to THINKING.
-            chopsticks[ptr->seed % N] = chopsticks[(ptr->seed + 1) % N] = 1;
+            chopsticks[ptr->chops[0]] = chopsticks[ptr->chops[1]] = 1;
+            pthrerr = pthread_cond_broadcast(&chopsticks_cond[ptr->chops[0]]);
+            if (pthrerr != 0)
+                fatalerr("Clock", 0, "Condition b/cast failed\n");
+            pthrerr = pthread_cond_broadcast(&chopsticks_cond[ptr->chops[1]]);
+            if (pthrerr != 0)
+                fatalerr("Clock", 0, "Condition b/cast failed\n");
             state = THINKING;
         }
+        printf("Thread id: %d, done clock, STATE: %d\n", pthread_self(), state);
         pthrerr = pthread_mutex_unlock(ptr->mutex);
         if (pthrerr != 0)
             fatalerr("Client", pthrerr, "Mutex unlock failed\n");
@@ -84,6 +106,7 @@ void *clk(void *vptr) {
     if (pthrerr != 0)
         fatalerr("Clock", pthrerr, "Mutex lock failed\n");
     for (tick = 0; tick < ptr->T; tick++) {
+        printf("Clock: %d\n", tick);
         while (nblocked < ptr-> N - 1) {
             pthrerr = pthread_cond_wait(ptr->chair, ptr->mutex);
             if (pthrerr != 0)
@@ -113,6 +136,23 @@ int main(int argc, char **argv) {
 
     nblocked = 0;
 
+    /* Get Arguements */
+    while (i < argc - 1) {
+        if (strncmp("-L", argv[i], strlen(argv[i])) == 0)
+            lam = atof(argv[++i]);
+        else if (strncmp("-M", argv[i], strlen(argv[i])) == 0)
+            mu = atof(argv[++i]);
+        else if (strncmp("-T", argv[i], strlen(argv[i])) == 0)
+            T = atoi(argv[++i]);
+        else if (strncmp("-N", argv[i], strlen(argv[i])) == 0)
+            N = atoi(argv[++i]);
+        else
+            fatalerr(argv[i], 0, "Invalid argument\n");
+        i++;
+    }
+    if (i != argc)
+        fatalerr(argv[0], 0, "Odd number of args\n");
+
     /* Initializer conditions and mutex */
     pthrerr = pthread_cond_init(&start_line, NULL);
     if (pthrerr != 0)
@@ -123,6 +163,17 @@ int main(int argc, char **argv) {
     pthrerr = pthread_mutex_init(&mutex, NULL);
     if (pthrerr != 0)
         fatalerr(argv[0], 0, "Initialization failed\n");
+    for(i=0; i < N; i++){
+        pthrerr = pthread_cond_init(&chopsticks_cond[i], NULL);
+        if (pthrerr != 0)
+            fatalerr(argv[0], 0, "Initialization failed\n");
+    }
+    for(i=0; i < N - 1; i++){
+        pthrerr = pthread_cond_init(&chairs_cond[i], NULL);
+        if (pthrerr != 0)
+            fatalerr(argv[0], 0, "Initialization failed\n");
+    }
+    printf("Working \n");
 
     /* Create args for philosophers */
     allargs = (struct thread_arg *)
@@ -133,11 +184,7 @@ int main(int argc, char **argv) {
             malloc(N * sizeof(pthread_t));
     if (alltids == NULL)
         fatalerr(argv[0], 0, "Out of memory\n");
-
-    allargs[0].N = N;
-    allargs[0].T = T;
-    allargs[0].lam = lam;
-    allargs[0].mu = mu;
+    
     allargs[0].mutex = &mutex;
     allargs[0].start_line = &start_line;
     allargs[0].chair = &chair;
@@ -147,6 +194,8 @@ int main(int argc, char **argv) {
     for (i = 0; i < N; i++) {
         allargs[i] = allargs[0];
         allargs[i].seed += i;
+        allargs[i].chops[0] = i % N;
+        allargs[i].chops[1] = (i + 1) % N;
         pthrerr = pthread_create(alltids + i, NULL, philosopher, allargs + i);
         if (pthrerr != 0)
             fatalerr(argv[0], pthrerr, "Philosopher creation failed\n");
